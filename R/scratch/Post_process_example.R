bf0320c1b28f9ae95ba5c14f40e96ed29c1ef69a
DuncanJurayj <duncan_jurayj@brown.edu>
2023-04-03 10:13:30 -0400

    from now on I'll be using sourced_post_processing_duncan.R but I added a 'date_of_run' paramater to the preprocessing() function which renames the generic SMR output based on the date of the run.

--- R/scratch/Post_process_example.R
+++ R/scratch/Post_process_example.R
@@ -15,9 +15,10 @@ getwd()
 
 VaM_data <- preprocessing(
   "./raw_data/discharge_historical/USGSdischarge.csv",
-  "./raw_data/smr_output/mfc_mb_2023-03-31.csv",
+  "./raw_data/smr_output/MFC_mass_balance_79.csv",
   "1965-10-01",
   "1970-10-01",
+  "2023-04-03",
   c('wshed_id', 'date', 'year', 'runoff_cm', 'precip_cm', 'rain_cm', 'actualET_flow_cm', 'canopy_evap_cm', 'snowmelt_cm', 'storage_amt_cm', 'throughfall_cm', 'canopy_storage_amt_cm', 'perc_cm', 'Q', 'swe_cm', 'condens_cm', 'snow_cm', 'baseflow', 'srad', 'latent', 'sensible', 'lw', 'q_rain_ground', 'q_total', 'ice_content', 'liquid_water', 'refreeze', 'vap_d_air', 'vap_d_snow')
 )
 
@@ -31,4 +32,4 @@ annual_fluxes(VaM_data, flux_vector)
 radiation_ts(VaM_data)
 SAM_check(VaM_data)
 Q_comparison(VaM_data)
-
+q.latent_debug(VaM_data)
